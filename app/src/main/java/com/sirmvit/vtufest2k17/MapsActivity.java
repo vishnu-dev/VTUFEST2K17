67525fbc1d90618c8243e938b5e6d204e6f6f331
mayankmethad <mayankmethad@gmail.com>
2017-02-24 14:46:43 +0530

    Added map style

--- app/src/main/java/com/sirmvit/vtufest2k17/MapsActivity.java
+++ app/src/main/java/com/sirmvit/vtufest2k17/MapsActivity.java
@@ -1,7 +1,9 @@
 package com.sirmvit.vtufest2k17;
 
+import android.content.res.Resources;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -9,12 +11,14 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    public static final String TAG = MapsActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,7 +44,22 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sir MVIT and move the camera
+        //style Map
+        try {
+            // Customise the styling of the base map using a JSON object defined
+            // in a raw resource file.
+            boolean success = googleMap.setMapStyle(
+                    MapStyleOptions.loadRawResourceStyle(
+                            this, R.raw.style_json));
+
+            if (!success) {
+                Log.e(TAG, "Style parsing failed.");
+            }
+        } catch (Resources.NotFoundException e) {
+            Log.e(TAG, "Can't find style. Error: ", e);
+        }
+
+        // Add a marker
         LatLng LOC_MVIT = new LatLng(13.15103d, 77.610929d); //gate
         LatLng LOC_SCIENCE = new LatLng(13.150812d, 77.609026d); //sci block
         LatLng LOC_NB = new LatLng(13.150893d, 77.610195d); //new block
@@ -49,17 +68,16 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng LOC_LIB = new LatLng(13.151283d, 77.608933d); // lib
         LatLng LOC_PL = new LatLng(13.150273d, 77.609734d); // parking ground
         LatLng LOC_CS = new LatLng(13.151042d, 77.608881d); //coffee
-        LatLng LOC_CA = new LatLng(13.149867d, 77.610226d); //atm
+        LatLng LOC_RS = new LatLng(13.149867d, 77.610226d); //atm
         LatLng LOC_ME = new LatLng(13.150604d, 77.6084d); //mech block
         LatLng LOC_CB = new LatLng(13.150856d, 77.60869d); //civil
         LatLng LOC_MBA = new LatLng(13.151316d, 77.609377d); //mba
         LatLng LOC_DEN = new LatLng(13.149771d, 77.608481d); //dental
         LatLng LOC_WORK = new LatLng(13.151112d, 77.607833d); //Workshop
         LatLng LOC_GROUND = new LatLng(13.14973d, 77.605473d); //Ground
-        //sugarcane juice shop
-        //Rolls
-        //Stationary
-        //canteen
+        LatLng LOC_SUGAR = new LatLng(13.149946d,77.609871d); // Sugarcane
+        LatLng LOC_ATM = new LatLng(13.149824d,77.610034d);//Rolls & Stationary
+        LatLng LOC_CANT = new LatLng(13.149910d,77.610383d); //canteen
         mMap.addMarker(new MarkerOptions().position(LOC_MVIT).title("MVIT"));
         mMap.addMarker(new MarkerOptions().position(LOC_SCIENCE).title("Science Block"));
         mMap.addMarker(new MarkerOptions().position(LOC_NB).title("New Block"));
@@ -68,13 +86,16 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.addMarker(new MarkerOptions().position(LOC_LIB).title("Library"));
         mMap.addMarker(new MarkerOptions().position(LOC_PL).title("Parking Lot"));
         mMap.addMarker(new MarkerOptions().position(LOC_CS).title("Coffee Shop"));
-        mMap.addMarker(new MarkerOptions().position(LOC_CA).title("ATM"));
+        mMap.addMarker(new MarkerOptions().position(LOC_RS).title("Rolls Corner & Stationary Shop"));
         mMap.addMarker(new MarkerOptions().position(LOC_ME).title("Mechanical Block"));
         mMap.addMarker(new MarkerOptions().position(LOC_CB).title("Civil Block"));
         mMap.addMarker(new MarkerOptions().position(LOC_MBA).title("MBA Block"));
         mMap.addMarker(new MarkerOptions().position(LOC_DEN).title("Dental Block"));
         mMap.addMarker(new MarkerOptions().position(LOC_WORK).title("WorkShop"));
         mMap.addMarker(new MarkerOptions().position(LOC_GROUND).title("Grounds"));
+        mMap.addMarker(new MarkerOptions().position(LOC_SUGAR).title("Sugarcane Juice Shop"));
+        mMap.addMarker(new MarkerOptions().position(LOC_ATM).title("ATM"));
+        mMap.addMarker(new MarkerOptions().position(LOC_CANT).title("Canteen"));
         CameraPosition cameraPosition = new CameraPosition.Builder()
                 .target(LOC_MVIT)
                 .zoom(18)
