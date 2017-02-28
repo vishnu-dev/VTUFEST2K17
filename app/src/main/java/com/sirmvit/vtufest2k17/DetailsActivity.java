8ab08ff4fa37a1c5e6f0bafdcadd434f3132bdb4
Akhilsudh <akhil.sudh@gmail.com>
2017-02-28 22:40:35 +0530

    Added collapsing toolbar

--- app/src/main/java/com/sirmvit/vtufest2k17/DetailsActivity.java
+++ app/src/main/java/com/sirmvit/vtufest2k17/DetailsActivity.java
@@ -1,7 +1,11 @@
 package com.sirmvit.vtufest2k17;
 
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class DetailsActivity extends AppCompatActivity {
 
@@ -10,7 +14,11 @@ public class DetailsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
 
+
+        CollapsingToolbarLayout mActionBarToolbar = (CollapsingToolbarLayout) findViewById(R.id.collapsingToolbar);
+        mActionBarToolbar.setTitle(MapsActivity.Data.getTitle());
+
         //This is done to change the title of the activity depending on the marker selected.
-        setTitle(MapsActivity.Data.getTitle());
+        //setTitle(MapsActivity.Data.getTitle());
     }
 }
